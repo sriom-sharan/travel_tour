6099144cc1a355197fe7ed6c0c41ce47a6311440
sriom-sharan <sriomsharan.personal@gmail.com>
2025-05-17 16:31:43 +0530

    bug fix

--- src/App.tsx
+++ src/App.tsx
@@ -5,23 +5,23 @@ import { Button } from "./components/ui/button";
 
 function App() {
   return (
-    <div className="bg-white w-full ">
+    <div className="bg-white w-full h-full">
       <Navbar />
-      <div className="flex lg:flex-row flex-col w-full h-[110vh] mt-[79px]">
-        <div className="flex-1 h-1/2 lg:h-full">
+      <div className="flex md:flex-row flex-col w-full  mt-[79px] ">
+        <div className="flex-1 h-1/2  md:h-full">
           <picture className="w-full lg:h-full ">
-            <source className="lg:object-cover object-cover" srcSet={heroAvif} type="image/avif" />
+            {/* <source className="lg:object-cover object-cover" srcSet={heroAvif} type="image/avif" /> */}
             <img
               src={heroJpg}
               alt="Hero Image"
-              className="w-full h-full object-cover " // ✅ this makes it work
+              className="w-full h-full object-cover" 
             />
           </picture>
         </div>
-        <div className="flex items-center font-spartan flex-col lg:gap-8 gap-4 justify-center h-1/2 lg:h-full flex-1  lg:p-10">
-          <h1 className="text-8xl text-center">Discover Your Destination</h1>
-          <p className="font-normal text-xl">Crafting Tailored Travel Experiences</p>
-          <Button className="bg-transparent font-normal w-80 py-6  rounded-full border-black text-normal text-xl" variant={"outline"}>Explore</Button>
+        <div className="flex items-center font-spartan flex-col lg:gap-8 gap-4 justify-center my-auto min-h-1/2  lg:h-full flex-1 p-4 py-6 lg:p-10 ">
+          <h1 className="lg:text-7xl xl:text-8xl md:text-6xl text-5xl text-center">Discover Your Destination</h1>
+          <p className="font-normal md:text-xl">Crafting Tailored Travel Experiences</p>
+          <Button className="bg-transparent font-normal md:w-80 w-48 py-6  rounded-full border-black text-normal md:text-xl" variant={"outline"}>Explore</Button>
         </div>
       </div>
     </div>
