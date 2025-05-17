3cba10d5c5f2681102d2224c79d4386b3bed80f2
sriom-sharan <sriomsharan.personal@gmail.com>
2025-05-17 15:21:07 +0530

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,31 @@
+import Navbar from "./components/Navbar";
+import heroAvif from "./assets/hero.avif";
+import heroJpg from "./assets/hero.jpg";
+import { Button } from "./components/ui/button";
+
+function App() {
+  return (
+    <div className="bg-white w-full ">
+      <Navbar />
+      <div className="flex lg:flex-row flex-col w-full h-[110vh] mt-[99px]">
+        <div className="flex-1 h-1/2 lg:h-full">
+          <picture className="w-full lg:h-full ">
+            <source className="lg:object-cover object-cover" srcSet={heroAvif} type="image/avif" />
+            <img
+              src={heroJpg}
+              alt="Hero Image"
+              className="w-full h-full object-cover " // ✅ this makes it work
+            />
+          </picture>
+        </div>
+        <div className="flex items-center font-spartan flex-col lg:gap-8 gap-4 justify-center h-1/2 lg:h-full flex-1  lg:pt-10">
+          <h1 className="text-8xl text-center">Discover Your Destination</h1>
+          <p className="font-normal text-3xl">Crafting Tailored Travel Experiences</p>
+          <Button className="bg-transparent font-normal w-80 py-8 mt-8 rounded-full border-black text-normal text-2xl" variant={"outline"}>Explore</Button>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Navbar.tsx
@@ -0,0 +1,74 @@
+import React, { useState } from "react";
+
+const Navbar = () => {
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+
+  return (
+    <nav className="w-full fixed top-0 font-spartan left-0 z-50 h-[100px] bg-background">
+      <div className="max-w-screen-2xl flex flex-wrap h-28 items-center justify-between mx-auto p-4">
+        <a
+          href="https://flowbite.com/"
+          className="flex items-center space-x-3 rtl:space-x-reverse"
+        >
+          <img
+            src="https://flowbite.com/docs/images/logo.svg"
+            className="h-8"
+            alt="Flowbite Logo"
+          />
+          <span className="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">
+            Framing Memories
+          </span>
+        </a>
+
+        {/* Toggle button */}
+        <button
+          onClick={() => setIsMenuOpen(!isMenuOpen)}
+          type="button"
+          className="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2  dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600"
+          aria-controls="navbar-default"
+          aria-expanded={isMenuOpen}
+        >
+          <span className="sr-only">Open main menu</span>
+          <svg
+            className="w-5 h-5"
+            aria-hidden="true"
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 17 14"
+          >
+            <path
+              stroke="currentColor"
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              strokeWidth="2"
+              d="M1 1h15M1 7h15M1 13h15"
+            />
+          </svg>
+        </button>
+
+        {/* Menu */}
+        <div
+          className={`w-full md:block md:w-auto ${
+            isMenuOpen ? "block" : "hidden"
+          }`}
+          id="navbar-default"
+        >
+          <ul className="font-medium flex flex-col md:flex-1 p-4 md:p-0 md:gap-4 mt-4 absolute md:static w-full top-24 right-0  border bg-gray-200 md:bg-transparent  border-gray-100 rounded-lg md:flex-row md:justify-end md:space-x-10 rtl:space-x-reverse md:mt-0 md:border-0 dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
+            {["Home", "About", "Services", "Pricing", "Contact"].map((item) => (
+              <li key={item} className="p-4 md:border-transparent md:p-0 border-b-2 border-b-black">
+                <a
+                  href="#"
+                  className="block  px-4 font-normal text-2xl md:border-transparent text-gray-900 md:rounded-sm hover:text-blue-300 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent"
+                >
+                  {item}
+                </a>
+              </li>
+            ))}
+          </ul>
+        </div>
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
